5e515aea80f16e6a8867813f87af8f9eeb4d4b0f
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-04-21 12:23:31 -0300

    primeirot commit

---
+++ usandoframeworkbootle.py
@@ -0,0 +1,71 @@
+from bottle import route, run, template, static_file,get, post, request, redirect 
+import sys
+print(">>>>>>>>>>>>>>>", sys.getdefaultencoding())
+#from bottle import route, run, template, static_file, get, post, request, redirect  
+# usuariosautorizados={"Maria":"mariauser", "João":"joaouser","Antonio":"Antoniouser" }
+
+usuarios_autorizados = [
+    {"nome": "Maria", "username": "mariauser", "password": "teste123"},
+    {"nome": "João", "username": "João", "password": "teste123"},
+    {"nome": "Antonio", "username": "antoniouser", "password": "teste123"}
+]
+
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./views/static')
+
+
+@get('/')
+@route('/wrong/login')
+def wrong():
+    redirect("/right/login")
+
+
+
+@get('/login')  # or @route('/login')
+def login():
+  return template('login')
+
+
+@post('/login', method='POST')
+def do_login():
+
+    message = None
+
+    try:
+
+        username = request.forms.get('username', None)
+        password = request.forms.get('password', None)
+       
+                 
+       
+        if username=="":   
+              raise Exception("Campo username não informado")
+
+    except Exception as ex:
+       message = ex
+      
+
+    else:
+
+        usuario_autenticado = {}
+
+        for usuario in usuarios_autorizados:
+
+            if usuario.get("username") == username and usuario.get("password") == password:
+
+                usuario_autenticado["username"] = usuario["username"]
+                usuario_autenticado["nome"] = usuario["nome"]
+
+                break
+
+        if usuario_autenticado == {}:
+            message = "Login Falhou"
+
+    finally:
+
+        return template("index", usuario_autenticado) if message is None else template("erro", message=message)
+
+
+run(host='localhost', port=8080, debug=True)
---
+++ usandoframeworversanterior.py
@@ -0,0 +1,32 @@
+from bottle import route, run, template, static_file,get, post, request # or route
+
+usuariosautorizados={"Maria":"mariauser", "João":"joaouser","Antonio":"Antoniouser" }
+
+#@route('/static/<filename>') # rota achar arquivos estaticos
+@route('/hello/static/<filename>')
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./views/static')
+@route('/')
+@get('/login') # or @route('/login')
+def login():
+  return template('login')
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+        username = request.forms.get('username')
+        password = request.forms.get('password')
+        if username in  usuariosautorizados and password== usuariosautorizados[username]:
+             return template("index")
+        
+        else:
+             return "<p>Login Falhou</p>"
+run(host='localhost', port=8080, debug=True)
+  
+
+ 
+                 
+#navegador.get("http://localhost:8080/login")
+#https://bottlepy.org/docs/dev/tutorial.html#generating-content
+#http://localhost:8080/
+#http://localhost:8080/hello/ola
