0dc589884f820fbec0732b98996a0fa215b9d2d7
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-05-08 17:09:49 -0300

    site responsivo em andamento

--- views/static/horas.js
+++ views/static/horas.js
@@ -3,15 +3,15 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     var minuto =  data.getMinutes()
-    horas.innerHTML = `<h1>Agora são <strong> ${hora} horas e ${minuto} </strong>minutos</h1>`
+    horas.innerHTML = `<h3>Agora são <strong> ${hora} horas e ${minuto} </strong>minutos</h3>`
     if (hora >= 0 && hora < 12) {
-         horas.innerHTML +=`<h2>  Bom dia<h2>`
+         horas.innerHTML +=`<h3>  Bom dia<h3>`
          document.body.style.background="#e2cd9f"
     }else if (hora >=12 && hora < 18){
-         horas.innerHTML +=`<h2>  Boa Tarde </h2>`
+         horas.innerHTML +=`<h3>  Boa Tarde </h3>`
          document.body.style.background="rgb(203, 211, 218)"
     } else {
-         horas.innerHTML +=`<h2>  Boa Noite</h2>`
+         horas.innerHTML +=`<h3>  Boa Noite</h3>`
          document.body.style.background="rgb(243, 236, 236)"
     }
 }
